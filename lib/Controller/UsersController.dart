ca3272a47624a74ca1a90cd53740e933eceaf465
walidQasem <qasemwalide@gmail.com>
2023-12-01 18:32:31 +0100

    addLoadig 2023-12-01 18:32

--- lib/Controller/UsersController.dart
+++ lib/Controller/UsersController.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
 
 import '../Model/UsersModel.dart';
@@ -7,6 +8,8 @@ class UserController extends GetxController {
   List<User> users = [];
   List<User> filteredUsers = [];
   bool isFiltered = false;
+  bool isLoading = false;
+  Icon icon = const Icon(Icons.filter_alt_outlined);
 
   filterUsersByInterest(String interest) {
     filteredUsers =
@@ -21,16 +24,25 @@ class UserController extends GetxController {
         filterUsersByInterest(user.interests![indexUser]);
       }
     }
+    if (isFiltered) {
+      icon = const Icon(Icons.highlight_remove_sharp);
+    }
+    icon = const Icon(Icons.filter_alt_outlined);
     update();
   }
 
   void fetchUsers() async {
+    isLoading = true;
     QuerySnapshot querySnapshot =
         await FirebaseFirestore.instance.collection('users').get();
+
     users = querySnapshot.docs.map((doc) {
       Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
       return User.fromJson(data);
     }).toList();
+
+    isLoading = false;
+
     update();
   }
 
--- lib/Pages/UserPage.dart
+++ lib/Pages/UserPage.dart
@@ -19,36 +19,40 @@ class UserPage extends StatelessWidget {
             userController.onfilter();
           },
           child: GetBuilder<UserController>(builder: (controller) {
-            return controller.isFiltered == false
-                ? const Icon(Icons.filter_alt_outlined)
-                : const Icon(Icons.highlight_remove_sharp);
+            return controller.icon;
           }),
         ),
         body: GetBuilder<UserController>(builder: (controller) {
-          if (controller.isFiltered) {
+          if (controller.isLoading) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else {
+            if (!controller.isFiltered) {
+              return ListView.builder(
+                itemCount: controller.users.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return UserComponent(
+                      color: const Color.fromARGB(255, 237, 237, 237),
+                      id: controller.users[index].id!,
+                      name: controller.users[index].name.toString(),
+                      friends: controller.users[index].friends!,
+                      interests: controller.users[index].interests!);
+                },
+              );
+            }
             return ListView.builder(
-              itemCount: controller.users.length,
+              itemCount: controller.filteredUsers.length,
               itemBuilder: (BuildContext context, int index) {
                 return UserComponent(
                     color: const Color.fromARGB(255, 237, 237, 237),
-                    id: controller.users[index].id!,
-                    name: controller.users[index].name.toString(),
-                    friends: controller.users[index].friends!,
-                    interests: controller.users[index].interests!);
+                    id: controller.filteredUsers[index].id!,
+                    name: controller.filteredUsers[index].name.toString(),
+                    friends: controller.filteredUsers[index].friends!,
+                    interests: controller.filteredUsers[index].interests!);
               },
             );
           }
-          return ListView.builder(
-            itemCount: controller.filteredUsers.length,
-            itemBuilder: (BuildContext context, int index) {
-              return UserComponent(
-                  color: const Color.fromARGB(255, 237, 237, 237),
-                  id: controller.filteredUsers[index].id!,
-                  name: controller.filteredUsers[index].name.toString(),
-                  friends: controller.filteredUsers[index].friends!,
-                  interests: controller.filteredUsers[index].interests!);
-            },
-          );
         }));
   }
 }
